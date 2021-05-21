f21c669fc4263ab595ba2f0be3e8a5a4c10e006d
Berzaf <70249457+Berzaf@users.noreply.github.com>
2021-05-21 15:20:04 -0500

    Add files via upload
    components

---
+++ src/components/aboutme.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+
+class About extends Component {
+    render() {
+        return(
+            <div className="about-body">
+                <div className="about-content">
+
+                    <h1>About me</h1>
+                    <div className="row">
+
+                        <div className="col-lg-12 col-lg-offset-2">
+                            <p className="about-me">I'm frontend developer. I have the skills and training needed to help develop and maintain websites and to make improvements that will enhance functionality.
+                            I am an excellent team player with strong attention to detail and an important ability to work and perform well under pressure. I have an extensive knowledge of JavaScript, React, C#, PHP and HTML5.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+        )
+    }
+}
+export default About;
